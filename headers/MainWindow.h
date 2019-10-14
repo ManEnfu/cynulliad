686c752e1bc138d18b4b3fcc0cf7c6f0a52ff3c8
ManEnfu <hxrd97@gmail.com>
2019-10-14 22:54:15 +0700

    Add placeholder GUI

--- headers/MainWindow.h
+++ headers/MainWindow.h
@@ -3,13 +3,21 @@
 
 #include <QWidget>
 #include <QLabel>
+#include <QTextEdit>
 
 class MainWindow: public QWidget {
 private:
     QLabel* text1;
+    QLabel* text2;
+    QLabel* text3;
+    QTextEdit* textbox1;
+    QTextEdit* textbox2;
+    QTextEdit* textbox3;
 public:
     MainWindow(QWidget *parent, int width, int height);
     void initWindow(int width, int height);
+    void resizeEvent(QResizeEvent *re);
+    void updateSize();
     ~MainWindow();
 };
 
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -12,10 +12,35 @@ void MainWindow::initWindow(int width, int height) {
         width, height);
     this->setWindowTitle("Cynulliad");
 
-    text1 = new QLabel("Assembly code:");
-    text1->move(10, 10);
+    text1 = new QLabel("Assembly Code");
     text1->setParent(this);
+    text2 = new QLabel("Instruction Sequence");
+    text2->setParent(this);
+    text3 = new QLabel("Registers and Memories");
+    text3->setParent(this);
+
+    textbox1 = new QTextEdit();
+    textbox1->setParent(this);
+    textbox2 = new QTextEdit();
+    textbox2->setParent(this);
+    textbox3 = new QTextEdit();
+    textbox3->setParent(this);
+    this->updateSize();
     delete desktop;
+}
+
+void MainWindow::updateSize() {
+    int grid = this->width() / 3;
+    text1->move(10, 10);
+    textbox1->setGeometry(10, 30, grid - 20, 500);
+    text2->move(grid  + 10, 10);
+    textbox2->setGeometry(grid + 10, 30, grid - 20, 500);
+    text3->move(2 * grid + 10, 10);
+    textbox3->setGeometry(2 * grid + 10, 30, grid - 20, 500);
+}
+
+void MainWindow::resizeEvent(QResizeEvent *re) {
+    this->updateSize();
 }
 
 MainWindow::~MainWindow() {
